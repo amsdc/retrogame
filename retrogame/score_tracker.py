d50216302cbd02e6347ef391750504b1b75a7f75
Chandran.K <chandran.k.2009.gmail.com>
2022-07-01 12:45:22 +0530

    Added getters for score and lives

--- retrogame/score_tracker.py
+++ retrogame/score_tracker.py
@@ -8,6 +8,12 @@ class Score:
         print(FILE_PATH)
         self.file = open(FILE_PATH, "w")
 
+    def getLives(self):
+        return self.score__["lives"]
+
+    def getScore(self):
+        return self.score__["score"]
+
     def saveScore(self):
         json_score = json.dumps(self.score__, indent=3)
         self.file.write(json_score)
