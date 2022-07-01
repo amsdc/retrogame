00bcc8eab8c87f96548c695d23e64c11ada4fcbd
Chandran.K <chandran.k.2009.gmail.com>
2022-07-01 20:38:56 +0530

    Updated score_tracker

--- retrogame/score_tracker.py
+++ retrogame/score_tracker.py
@@ -1,42 +1,54 @@
 import json
+import os.path
+
 from retrogame.constants import FILE_PATH
 
 
 class Score:
     def __init__(self):
-        self.score = {"lives": 3, "score": 0}
-        self.file =  open(FILE_PATH, "w")
+
+        self.__lives=3
+        self.file = open(FILE_PATH, "w+")
+        self.dictscore = json.load(self.file) if self.file.read() else {"Highscore": 0}
+
+        self.__score = 0
 
     def getLives(self):
-        return self.score["lives"]
+        return self.__lives
+
 
     def getScore(self):
-        return self.score["score"]
+        return self.__score
 
     def save_score(self):
-        json_score = json.dumps(self.score, indent=3)
-        self.file.write(json_score)
+        self.dictscore = json.load(self.file) if self.file.read() else {"Highscore": 0}
+        print(self.__score, self.dictscore["Highscore"])
+        if self.__score > self.dictscore["Highscore"]:
+            print("cond")
+            self.dictscore["Highscore"] = self.__score
+
+        json.dump(self.dictscore, self.file)
 
     def close_file(self):
         self.file.close()
 
     def add_point(self):
-        self.score["score"] += 1
+        self.__score += 1
 
     def rem_point(self):
-        if self.score["score"] >= 0:
-            self.score["score"] -= 1
+        if self.__score >= 0:
+            self.__score -= 1
         else:
             return False
 
     def rem_life(self):
-        if self.score["lives"] >= 0:
-            self.score["lives"] -= 1
+        if self.__lives >= 0:
+            self.__lives -= 1
         else:
             return False
 
     def add_life(self):
-        if (self.score["lives"] <= 3):
-            self.score["lives"] += 1
+        if self.__lives <= 3:
+            self.__lives += 1
         else:
             return False
