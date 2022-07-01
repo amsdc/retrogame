a87dd07c5b185045fbb43964053fd110e568407d
Chandran.K <chandran.k.2009.gmail.com>
2022-07-01 12:37:59 +0530

    Added score tracking
    Added score tracking, score removal, score saving and counters for points and lives

--- retrogame/constants.py
+++ retrogame/constants.py
@@ -1,2 +1,3 @@
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+FILE_PATH = r"C:\Users\KChandran\Desktop\retrogame\retro_score.json"
---
+++ retrogame/score_tracker.py
@@ -0,0 +1,37 @@
+import json
+from retrogame.constants import FILE_PATH
+
+
+class Score:
+    def __init__(self):
+        self.score__ = {"lives": 3, "score": 0}
+        print(FILE_PATH)
+        self.file = open(FILE_PATH, "w")
+
+    def saveScore(self):
+        json_score = json.dumps(self.score__, indent=3)
+        self.file.write(json_score)
+
+    def closeFile(self):
+        self.file.close()
+
+    def add_point(self):
+        self.score__["score"] += 1
+
+    def rem_point(self):
+        if self.score__["score"] >= 0:
+            self.score__["score"] -= 1
+        else:
+            return False
+
+    def rem_life(self):
+        if (self.score__["lives"] >= 0):
+            self.score__["lives"] -= 1
+        else:
+            return False
+
+    def add_life(self):
+        if (self.score__["lives"] <= 3):
+            self.score__["lives"] += 1
+        else:
+            return False
