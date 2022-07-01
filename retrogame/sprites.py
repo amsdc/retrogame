e6f5a567c020c1bc0ae9ce2d45fd95f1a4893817
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2022-07-01 12:05:09 +0530

    Update sprites.py

--- retrogame/sprites.py
+++ retrogame/sprites.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 from pygame.locals import (
@@ -55,3 +57,31 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT-self.p_bottom_padding:
             self.rect.bottom = SCREEN_HEIGHT-self.p_bottom_padding
+
+
+class Stone(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.player_width = 75
+        self.player_height = 75
+        
+        self.s_left_padding = 5
+        self.s_right_padding = 5
+        self.s_bottom_padding = 5
+        
+        self.surf = pygame.Surface((self.player_width, self.player_height))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(0+self.s_left_padding, SCREEN_WIDTH-self.s_right_padding),
+                random.randint(-20, -100),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
